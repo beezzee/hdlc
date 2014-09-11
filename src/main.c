c56e90a98cf1b67332aabec9b7be556044db5b85
Peter Günther <peter.guenther@uni-paderborn.de>
2014-09-11 17:54:21 +0200

    replace addresses of interrupt vectors

--- src/main.c
+++ src/main.c
@@ -157,11 +157,11 @@ void ADC12ISR(void)
 {
         static uint8_t index = 0;
 
-        switch (__even_in_range(ADC12IV, 34)) {
-        case  0: break;         //Vector  0:  No interrupt
-        case  2: break;         //Vector  2:  ADC overflow
-        case  4: break;         //Vector  4:  ADC timing overflow
-        case  6:                //Vector  6:  ADC12IFG0
+        switch (ADC12IV) {
+        case  ADC12IV_NONE: break;         //Vector  0:  No interrupt
+        case  ADC12IV_ADC12OVIFG: break;         //Vector  2:  ADC overflow
+        case  ADC12IV_ADC12TOVIFG: break;         //Vector  4:  ADC timing overflow
+        case  ADC12IV_ADC12IFG0:                //Vector  6:  ADC12IFG0
                 //Move results
                 results[index] =
                         ADC12_A_getResults(ADC12_A_BASE,
@@ -173,20 +173,21 @@ void ADC12ISR(void)
 
                 if (index == 8)
                         index = 0;
-        case  8: break;         //Vector  8:  ADC12IFG1
-        case 10: break;         //Vector 10:  ADC12IFG2
-        case 12: break;         //Vector 12:  ADC12IFG3
-        case 14: break;         //Vector 14:  ADC12IFG4
-        case 16: break;         //Vector 16:  ADC12IFG5
-        case 18: break;         //Vector 18:  ADC12IFG6
-        case 20: break;         //Vector 20:  ADC12IFG7
-        case 22: break;         //Vector 22:  ADC12IFG8
-        case 24: break;         //Vector 24:  ADC12IFG9
-        case 26: break;         //Vector 26:  ADC12IFG10
-        case 28: break;         //Vector 28:  ADC12IFG11
-        case 30: break;         //Vector 30:  ADC12IFG12
-        case 32: break;         //Vector 32:  ADC12IFG13
-        case 34: break;         //Vector 34:  ADC12IFG14
+        case  ADC12IV_ADC12IFG1: break;         
+        case ADC12IV_ADC12IFG2: break;         
+        case ADC12IV_ADC12IFG3: break;         
+        case ADC12IV_ADC12IFG4: break;         
+        case ADC12IV_ADC12IFG5: break;         
+        case ADC12IV_ADC12IFG6: break;         
+        case ADC12IV_ADC12IFG7: break;         
+        case ADC12IV_ADC12IFG8: break;         
+        case ADC12IV_ADC12IFG9: break;         
+        case ADC12IV_ADC12IFG10: break;         
+        case ADC12IV_ADC12IFG11: break;         
+        case ADC12IV_ADC12IFG12: break;         
+        case ADC12IV_ADC12IFG13: break;         
+        case ADC12IV_ADC12IFG14: break;         
+        case ADC12IV_ADC12IFG15: break;         
         default: break;
         }
 }
