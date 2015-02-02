f61808a257c8ed3052970ec133af21be8cabf899
Peter Günther <peter.guenther@uni-paderborn.de>
2015-02-02 14:14:17 +0100

    add parenthesis to macro

--- src/msp_utils.h
+++ src/msp_utils.h
@@ -4,6 +4,6 @@
 /**
    Read two consecutive bytes in little-endian format and return as uint16_t.
 */
-#define uint16_from_little_endian(p) (*((uint16_t *) p))
+#define uint16_from_little_endian(p) (*((uint16_t *) (p)))
 
 #endif //_msp_utils_h
